e33b18b860613bb8a8a5d0a2679c4e357b5331f7
Moktadir Rahman <autocircled@gmail.com>
2022-03-27 22:55:40 +0600

    multiple accordion issue solved

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,22 +6,24 @@ AFBFront.accordion = {
     },
 
     create: function(){
-        const afb_box = document.querySelector(".a-faq-builder .afb-items");
-        const afb_items = afb_box.querySelectorAll('li.afb-item');
-        afb_items.forEach(( el, i) => {
-            const afb_item_title = el.querySelector('a.afb-item-title');
-            afb_item_title.addEventListener('click', function(event){
-                event.preventDefault();
-                if( ! AFB_DATA.multi_open ) {
-                    for( const item of afb_items ){
-                        if( el !== item && item.classList.contains( 'active' ) ) {
-                            item.classList.remove( 'active' );
+        const afb_boxs = document.querySelectorAll(".a-faq-builder .afb-items");
+        afb_boxs.forEach(( afb_box ) => {
+            const afb_items = afb_box.querySelectorAll('li.afb-item');
+            afb_items.forEach(( el, i) => {
+                const afb_item_title = el.querySelector('a.afb-item-title');
+                afb_item_title.addEventListener('click', function(event){
+                    event.preventDefault();
+                    if( ! AFB_DATA.multi_open ) {
+                        for( const item of afb_items ){
+                            if( el !== item && item.classList.contains( 'active' ) ) {
+                                item.classList.remove( 'active' );
+                            }
                         }
                     }
-                }
-                el.classList.toggle('active');
+                    el.classList.toggle('active');
+                });
+    
             });
-
         });
 
     }
