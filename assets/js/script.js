7880ba22eccb662a31cc01198ac6cd602ca83200
Moktadir Rahman <autocircled@gmail.com>
2022-05-12 14:08:05 +0600

    I am working on accordion design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,33 @@
 var AFBFront = AFBFront || {};
 
 AFBFront.accordion = {
-    init: function(){
-        this.create();
-    },
+	init: function () {
+		this.create();
+	},
 
-    create: function(){
-        const afb_boxs = document.querySelectorAll(".a-faq-builder .afb-items");
-        afb_boxs.forEach(( afb_box ) => {
-            const afb_items = afb_box.querySelectorAll('li.afb-item');
-            afb_items.forEach(( el, i) => {
-                const afb_item_title = el.querySelector('a.afb-item-title');
-                afb_item_title.addEventListener('click', function(event){
-                    event.preventDefault();
-                    if( ! AFB_DATA.multi_open ) {
-                        for( const item of afb_items ){
-                            if( el !== item && item.classList.contains( 'active' ) ) {
-                                item.classList.remove( 'active' );
-                            }
-                        }
-                    }
-                    el.classList.toggle('active');
-                });
-    
-            });
-        });
-
-    }
+	create: function () {
+		const afb_boxs = document.querySelectorAll(".a-faq-builder .afb-items");
+		afb_boxs.forEach((afb_box) => {
+			const afb_items = afb_box.querySelectorAll("li.afb-item");
+			afb_items.forEach((el, i) => {
+				const afb_item_title = el.querySelector("a.afb-item-title");
+				afb_item_title.addEventListener("click", function (event) {
+					event.preventDefault();
+					if (!AFB_DATA.multi_open) {
+						for (const item of afb_items) {
+							if (
+								el !== item &&
+								item.classList.contains("active")
+							) {
+								item.classList.remove("active");
+							}
+						}
+					}
+					el.classList.toggle("active");
+				});
+			});
+		});
+	},
 };
 
 /**
@@ -38,19 +39,21 @@ AFBFront.accordion = {
  *
  * @param {Function} fn Callback function to run.
  */
- function AFBFrontDomReady( fn ) {
-	if ( typeof fn !== 'function' ) {
+function AFBFrontDomReady(fn) {
+	if (typeof fn !== "function") {
 		return;
 	}
 
-	if ( document.readyState === 'interactive' || document.readyState === 'complete' ) {
+	if (
+		document.readyState === "interactive" ||
+		document.readyState === "complete"
+	) {
 		return fn();
 	}
 
-	document.addEventListener( 'DOMContentLoaded', fn, false );
+	document.addEventListener("DOMContentLoaded", fn, false);
 }
 
-
-AFBFrontDomReady( function() {
+AFBFrontDomReady(function () {
 	AFBFront.accordion.init();
-} );
+});
